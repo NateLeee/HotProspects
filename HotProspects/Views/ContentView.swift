f8824ee904a96b36905d23045b24323c855bdd79
Nate Lee <nateleee.dev@gmail.com>
2020-08-12 21:54:06 +0800

    - [x] Manually publishing ObservableObject changes.

--- HotProspects/Views/ContentView.swift
+++ HotProspects/Views/ContentView.swift
@@ -8,61 +8,36 @@
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
-    case dataIsNil
-}
-
-
-struct ContentView: View {
-    @State private var text = "Hello, World!"
-    
-    var body: some View {
-        GeometryReader { geo in
-            ScrollView(.vertical) {
-                Text(self.text)
-                    .onAppear {
-                        self.fetchData(from: "https://www.apple.com") { (result) in
-                            switch result {
-                            case .failure(let error):
-                                self.text = error.localizedDescription
-                                
-                            case .success(let resultString):
-                                self.text = resultString
-                                
-                            }
-                        }
-                }
-                .frame(width: geo.size.width * 0.9)
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+        didSet {
+            if value >= 10 {
+                value = 0
             }
         }
+    }
+    
+    init() {
+        for i in 0 ... 10 {
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var delayedUpdater = DelayedUpdater()
+    
+    var body: some View {
+        Text("\(delayedUpdater.value)")
     }
     
     // Custom funcs
-    func fetchData(from urlString: String, handler completion: @escaping (Result<String, NetworkError>) -> Void) {
-        guard let url = URL(string: urlString) else {
-            completion(Result.failure(.badURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            DispatchQueue.main.async {
-                guard error == nil else {
-                    completion(Result.failure(.requestFailed))
-                    return
-                }
-                guard let data = data else {
-                    completion(Result.failure(.dataIsNil))
-                    return
-                }
-                // Otherwise, all is well
-                // Decode first
-                let stringData = String(decoding: data, as: UTF8.self)
-                completion(Result.success(stringData))
-            }
-        }.resume()
-        
-    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
