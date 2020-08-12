f2bde6ade40abffa0338e6baca7bc77e1b41fafc
Nate Lee <nateleee.dev@gmail.com>
2020-08-12 22:19:11 +0800

    - [x] Creating context menus.

--- HotProspects/Views/ContentView.swift
+++ HotProspects/Views/ContentView.swift
@@ -11,16 +11,45 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
+    @State private var backgroundColor = Color.red
     
     var body: some View {
-        Image("swiftui")
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(Color.black)
-            .edgesIgnoringSafeArea(.all)
+        VStack {
+            Text("Placeholder")
+                .padding()
+                .layoutPriority(1)
+                .padding()
+                .background(backgroundColor)
+            
+            Text("3D Touch Me")
+                .padding()
+                .contextMenu(ContextMenu(menuItems: {
+                    Button(action: {
+                        self.backgroundColor = .red
+                    }) {
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.white)
+                        Text("Red")
+                    }
+                    
+                    Button(action: {
+                        self.backgroundColor = .blue
+                    }) {
+                        Text("Blue")
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.blue)
+                    }
+                    
+                    Button(action: {
+                        self.backgroundColor = .yellow
+                    }) {
+                        Text("Yellow")
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.yellow)
+                    }
+                    
+                }))
+        }
     }
     
     // Custom funcs
