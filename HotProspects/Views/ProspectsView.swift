0256be4c02a974b9053e36b86541cb7f1f4e0c3d
Nate Lee <nateleee.dev@gmail.com>
2020-08-13 14:16:16 +0800

    - [x] Posting notifications to the lock screen.

--- HotProspects/Views/ProspectsView.swift
+++ HotProspects/Views/ProspectsView.swift
@@ -8,6 +8,8 @@
 
 import SwiftUI
 import CodeScanner
+import UserNotifications
+
 
 
 enum FilterType {
@@ -60,6 +62,12 @@ struct ProspectsView: View {
                         }) {
                             Text("\(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted")")
                         }
+                        
+                        if !prospect.isContacted {
+                            Button("Remind Me") {
+                                self.addNotification(for: prospect)
+                            }
+                        }
                     }
                 }
             }
@@ -95,6 +103,40 @@ struct ProspectsView: View {
         case .failure(let error):
             print("Scanning failed: \(error.localizedDescription)")
         }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+            // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("D'oh")
+                    }
+                }
+            }
+        }
+        
     }
     
 }
