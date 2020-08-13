948728d5cb7c121792d906fc53f71fb501611776
Nate Lee <nateleee.dev@gmail.com>
2020-08-13 21:56:57 +0800

    Added a Utilities class to get doc dir.

---
+++ HotProspects/Helpers/Utilities.swift
@@ -0,0 +1,17 @@
+//
+//  Utilities.swift
+//  HotProspects
+//
+//  Created by Nate Lee on 8/13/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
+
+class Utilities {
+    static func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
+    }
+}
