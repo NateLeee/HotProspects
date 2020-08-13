ba133a1c613aae9e0addcb459719aa4df6ab77ef
Nate Lee <nateleee.dev@gmail.com>
2020-08-13 12:21:33 +0800

    Added 2 new view files.

---
+++ HotProspects/Views/ProspectsView.swift
@@ -0,0 +1,17 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Nate Lee on 8/13/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
