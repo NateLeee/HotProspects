3337d96b2f39c4925926a459068611aab7ae8a9b
Nate Lee <nateleee.dev@gmail.com>
2020-08-13 13:59:55 +0800

    Added a new constants.swift file.

---
+++ HotProspects/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  HotProspects
+//
+//  Created by Nate Lee on 8/13/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let saveKey = "SavedData"
+}
