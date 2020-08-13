2c9627e41fe7bacfe98d4b80932b7535b92382f7
Nate Lee <nateleee.dev@gmail.com>
2020-08-13 13:03:54 +0800

    - [x] Generating and scaling up a QR code.

--- HotProspects/Views/MeView.swift
+++ HotProspects/Views/MeView.swift
@@ -7,11 +7,61 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
+
 
 struct MeView: View {
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "you@yoursite.com"
+    
+    let context = CIContext()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("My Info")) {
+                    TextField("Name", text: $name)
+                        .textContentType(.name)
+                    
+                    TextField("Email address", text: $emailAddress)
+                        .textContentType(.emailAddress)
+                }
+                
+                Section(header: Text("QR Code")) {
+                    HStack(alignment: .center) {
+                        Spacer()
+                        
+                        Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                            .interpolation(.none)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 135)
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .navigationBarTitle("Anout Me")
+        }
     }
+    
+    // Custom Funcs
+    func generateQRCode(from string: String) -> UIImage {
+        let data = string.data(using: String.Encoding.utf8)
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            
+            if let outputCIImage = filter.outputImage {
+                if let cgimg = context.createCGImage(outputCIImage, from: outputCIImage.extent) {
+                    return UIImage(cgImage: cgimg)
+                }
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle")!
+    }
+    
 }
 
 struct MeView_Previews: PreviewProvider {
